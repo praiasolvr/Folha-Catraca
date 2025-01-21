25f1fa1bc9bf7e614254e65c52d9ee4f826cd7c0
chawan <chawandossantos7@gmail.com>
2025-01-21 17:31:46 -0300

    botoes vistorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { FaBus } from "react-icons/fa6";
 
 function App() {
   // =========MINHAS VARIAVEIS========= //
@@ -22,24 +23,41 @@ function App() {
   // =========MINHAS FUNÇÕES========= //
   return (
     <div className='conteiner-folha-catraca'>
+      <div className='controladores-veiculos'>
+        
+        <button className='veiculos-vistoriados' onClick={() => { alert('ola') }}>
+          <FaBus />
+        Viculos vistoriados
+        </button>
+
+        <button className='veiculos-pensentes' onClick={() => { alert('ola') }}>
+          <FaBus />
+          Viculos pendentes
+        </button>
+      </div>
+
       <h1>Folha de catraca</h1>
       <div className="campoDate">
         <h2>Data:10/01/2025 </h2>
+
       </div>
       <form onSubmit={RegistrarCatraca}>
         <input
+          required
           type="number"
           placeholder='Numero veiculo'
           value={veiculo}
           onChange={(e) => { setVeiculo(e.target.value) }} />
 
         <input
+          required
           type="number"
           placeholder='Catrca fisica'
           value={catracaFisica}
           onChange={(e) => { setCatracaFisica(e.target.value) }} />
 
         <input
+          required
           type="number"
           placeholder='Catraca tacom'
           value={catracaTacom}
@@ -50,12 +68,12 @@ function App() {
           <p><strong>* </strong>Viagem fechada?</p>
           <div className='conteiner-resp-viagem-fechada'>
             <div>
-              <input type="radio" id="SIM-fechada" name="fechada" value="SIM-fechada" />
+              <input type="radio" id="SIM-fechada" name="fechada" value="SIM-fechada" required />
               <label for="SIM-fechada">SIM</label>
             </div>
 
             <div>
-              <input type="radio" id="NAO-fechada" name="fechada" value="NAO-fechada" />
+              <input type="radio" id="NAO-fechada" name="fechada" value="NAO-fechada" required />
               <label for="NAO-fechada">NÃO</label>
             </div>
           </div>
@@ -66,12 +84,12 @@ function App() {
           <p><strong>* </strong>Foi eu que fechei?</p>
           <div className='conteiner-resp-eu-fechei'>
             <div>
-              <input type="radio" id="SIM-fechei" name="fechei" value="SIM-fechei" />
+              <input type="radio" id="SIM-fechei" name="fechei" value="SIM-fechei" required />
               <label for="SIM-fechei">SIM</label>
             </div>
 
             <div>
-              <input type="radio" id="NAO-fechei" name="fechei" value="NAO-fechei" />
+              <input type="radio" id="NAO-fechei" name="fechei" value="NAO-fechei" required />
               <label for="NAO-fechei">NÃO</label>
             </div>
           </div>
